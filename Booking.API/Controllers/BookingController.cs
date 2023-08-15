fea67a04690faf7912eb5f0f4a7b98544af387b9
DESKTOP-I7RVKI7\Stefan <Stefan Roman>
2023-08-15 18:33:02 +0300

    Working at the functionality that validates the validation of credentials.

--- Booking.API/Controllers/BookingController.cs
+++ Booking.API/Controllers/BookingController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -19,9 +20,32 @@ namespace Booking.API.Controllers
                 return BadRequest("The content of the request cannot be null");
             #endregion
 
+            if (!UserIsValid(requestObject.Credentials))
+            {
+                throw new HttpResponseException(CreateUnauthorizedResponse())
+            };
 
             List<Offer> offers = new List<Offer>();
             return Content(HttpStatusCode.Created, offers);
+        }
+
+        private bool UserIsValid(Credentials credentials)
+        {
+            var validUserName = ConfigurationManager.AppSettings["ApiUser_UserName"].ToString();
+            var validPassword = ConfigurationManager.AppSettings["ApiUser_Password"].ToString();
+
+            if (credentials.UserName.Equals(validUserName) && credentials.Password.Equals(validPassword))
+                return true;
+
+            return false;
+        }
+
+        private HttpResponseMessage CreateUnauthorizedResponse()
+        {
+            return new HttpResponseMessage(HttpStatusCode.Unauthorized)
+            {
+                Content = new StringContent("Invalid username or password")
+            };
         }
     }
 }
